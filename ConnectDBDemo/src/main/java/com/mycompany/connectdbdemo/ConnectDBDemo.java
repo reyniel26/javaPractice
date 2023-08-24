1f4d63ba83cc14c1257dd463aa4dd5b7b2aad1e4
Reyniel Mark Escamillas <rmescamillas26@gmail.com>
2023-08-24 18:28:59 +0800

    Added demo

---
+++ ConnectDBDemo/src/main/java/com/mycompany/connectdbdemo/ConnectDBDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.connectdbdemo;
+
+/**
+ *
+ * @author rmesc
+ */
+public class ConnectDBDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
